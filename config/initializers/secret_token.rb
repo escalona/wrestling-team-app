62f49957235238adffc65833509c019dafaff502
Aaron Escalona <escalona.ap@gmail.com>
2012-12-18 19:31:29 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WrestlingTeamApp::Application.config.secret_token = '3bb942708ec27bc20800bdd88abbef0ccf0b5baa86d0f298426e7e432f15f8d27d2ac3a521004d8f9fe306017ef5105f2c4a3cc67b018bbac654ea861cdefccf'
