19fb9a6b691212ea15c22f54f23e31c8eedf8514
Aaron Escalona <escalona.ap@gmail.com>
2012-12-19 01:31:44 -0800

    Created index, create, and new actions in controller

--- app/controllers/wrestlers_controller.rb
+++ app/controllers/wrestlers_controller.rb
@@ -1,8 +1,29 @@
 class WrestlersController < ApplicationController
-  def index
 
+  def index
+    @wrestlers = Wrestler.all
+
+    respond_to do |format|
+      format.html # show default view
+      format.json { render :json => @wrestlers }
+    end
+  end
+
+  def create
+    @wrestler = Wrestler.new(params[:wrestler])
+
+    respond_to do |format|
+      if @wrestler.save
+        format.html { render :action => "create" }
+        format.json { render :json => @wrestler }
+      else
+        format.html { render :action => "new" }
+        format.json { render :json => @wrestler.errors, :status => unprocessable_entity }
+      end
+    end
   end
 
   def new
+    @wrestler = Wrestler.new
   end
 end
